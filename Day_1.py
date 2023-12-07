d558e4a18183b9f32294cb632bcd58a60d6a008a
ZakariaFatihiNoussi <ZakMegumiFatihi@proton.me>
2023-12-07 13:24:58 +0100

    testing whether there's an element without digits

--- Day_1.py
+++ Day_1.py
@@ -2,6 +2,11 @@
 
 with open("Day_1_input.txt", 'r') as Day_1_input :
     lines = [line.strip() for line in Day_1_input.readlines()]
+
+    for line in lines:
+        if any(char.isdigit() for char in line):
+            print("moving on")
+        else:
+            print(f'The lin "{line}" does not contain a digit.')
     
 
-print(lines)

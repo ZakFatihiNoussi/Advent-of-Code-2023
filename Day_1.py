4234aeda53854586feffe3494e2e25f40370ee73
ZakariaFatihiNoussi <ZakMegumiFatihi@proton.me>
2023-12-07 13:30:39 +0100

    single vs multiple digit per element count

--- Day_1.py
+++ Day_1.py
@@ -1,12 +1,20 @@
 
 
 with open("Day_1_input.txt", 'r') as Day_1_input :
-    lines = [line.strip() for line in Day_1_input.readlines()]
+    lines = [line.strip('\n') for line in Day_1_input.readlines()]
+
+    single_digits_count = 0
+    multiple_digits_count = 0
 
     for line in lines:
-        if any(char.isdigit() for char in line):
-            print("moving on")
-        else:
-            print(f'The lin "{line}" does not contain a digit.')
+        digit_count = sum(char.isdigit() for char in line)
+
+        if digit_count == 1:
+            single_digits_count += 1
+        elif digit_count > 1:
+            multiple_digits_count += 1
+
+print(f'Lines with one single digit: {single_digits_count}')
+print(f'Lines with more than one single digit: {multiple_digits_count}')
     
 

468874cbd9e8d4104b3a578c6e3ff9a83e32a5c0
ZakariaFatihiNoussi <ZakMegumiFatihi@proton.me>
2023-12-09 11:29:56 +0100

    replacing string digits with integer equivalent, unsolved case of 'twone'

--- Day_1.py
+++ Day_1.py
@@ -6,8 +6,16 @@ with open("Day_1_input.txt", 'r') as Day_1_input :
     single_digits_count = 0
     multiple_digits_count = 0
     sum_concatenated_digits = 0
-
+    number_mapping = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
+    
     for line in lines:
+        print(line)
+
+        for word, value in number_mapping.items():
+            line = line.replace(word, str(value))
+        print(line)
+        
+    
         digit_count = sum(char.isdigit() for char in line)
 
         if digit_count == 1:
@@ -17,23 +25,29 @@ with open("Day_1_input.txt", 'r') as Day_1_input :
 
         digit_indices = [i for i, char in enumerate(line) if char.isdigit()]
 
+
         if len(digit_indices) > 1:
             
             first_digit_index = digit_indices[0]
             last_digit_index = digit_indices[-1]
             result = line[first_digit_index] + line[last_digit_index]
             sum_concatenated_digits += int(result)
+            print(result, sum_concatenated_digits)
+            
 
         elif len(digit_indices) == 1:
             digit_index = digit_indices[0]
             digit = line[digit_index]
             result = digit * 2
             sum_concatenated_digits += int(result)
+            print(result, sum_concatenated_digits)
         
 
 
             
 print(f'Sum of concatenated digits: {sum_concatenated_digits}')
+
+
 print(f'Lines with one single digit: {single_digits_count}')
 print(f'Lines with more than one single digit: {multiple_digits_count}')
     
--- readme.md
+++ readme.md
@@ -2,7 +2,14 @@
 Choose different programming languages to solve the puzzles.
 
 ## Puzzle 1:
+#### Part 1:
 So, we have a text file in which:
 1. For each line, we want to extract the first and the last number, in case of one number, we want to replicate it.
 2. Concatenate the first and last digit into a two-digit decimal.
 3. Then sum up every two digit as the answer.
+
+#### Part 2:
+1. before the digit concatenation, we must find string numbers and convert them to their corresponding integer digit.
+2. Fix the case in which there are two string numbers attached, for example: twone
+2. 1. approve of the first string number "two", and dismiss "one" as "ne"
+
